234c82fa6617c4b79b7e60cb037b2e54f19179e8
= <=>
2022-10-15 14:29:49 -0400

    Added styling for beat

--- index.js
+++ index.js
@@ -2,27 +2,35 @@ let bpm = 90;
 let intervalIds = [];
 const hits = [];
 const audio = new Audio('tick.mp3');
+let numMeasures = 1;
+let measureSubdivision = 16;
 let counter = 0;
 setupRhythm();
 
 function setupRhythm() {
     const rhythmDiv = document.getElementById('rhythm');
-    for (let index = 0; index < 16; index++) {
-        const check = document.createElement('input');
-        check.setAttribute('type', 'checkbox');
-        check.setAttribute('id', 'rhythm' + index);
-        check.setAttribute('onchange', `toggledRhythm(this.checked, ${index})`);
-
-        if((index) % 4 === 0) {
-            check.setAttribute('checked', true);
-            hits[index] = true;
+    for (let measure = 0; measure < numMeasures; measure++) {
+        for (let subdivision = 0; subdivision < measureSubdivision; subdivision++) {
+            const index = subdivision * measure + subdivision;
+            const span = document.createElement('span');
+            const check = document.createElement('input');
+            check.setAttribute('type', 'checkbox');
+            span.setAttribute('id', 'rhythm' + index);
+            check.setAttribute('onchange', `toggledRhythm(this.checked, ${index})`);
+            
+            if((index) % 4 === 0) {
+                check.setAttribute('checked', true);
+                hits[index] = true;
+            }
+            else {
+                hits[index] = false;
+            }
+    
+            span.appendChild(check);
+            rhythmDiv.appendChild(span);
         }
-        else {
-            hits[index] = false;
-        }
-        rhythmDiv.appendChild(check);
-        console.log(hits);
     }
+    
 }
 
 function toggledRhythm(pressed, index) {
@@ -44,9 +52,16 @@ function stop() {
     counter = 0;
     intervalIds.forEach(id => clearInterval(id));
     intervalIds = [];
+    //clear on beat color
 }
 
 function playSound() {
+    let numSubdivisions = measureSubdivision * numMeasures;
+    let prevCounter = (counter + numSubdivisions - 1) % numSubdivisions;
+    let prevSpan = document.getElementById('rhythm' + prevCounter);
+    prevSpan?.setAttribute('class', '');
+    let span = document.getElementById('rhythm' + counter);
+    span.setAttribute('class', 'onBeat');
     if(hits[counter] === true) {
         audio.pause();
         audio.currentTime = 0;
@@ -59,7 +74,6 @@ function playSound() {
 function setBpm(newBpm) {
     bpm = newBpm;
     if(intervalIds.length > 0) {
-        console.log('resetting');
         start();
     }
 }
